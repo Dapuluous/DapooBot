d850f35b76e6676de10dc96cc44567991869cb7b
Dapuluous <mdhaffamahendra@gmail.com>
2020-09-27 16:32:19 +0700

    Changed stuff on misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -13,27 +13,48 @@ class Misc(commands.Cog):
 
 	@commands.command()
 	async def randomizeGroup(self, ctx, *, initialInput):
-		groupArray = initialInput.split(",")
+		groupArray = initialInput.split(", ")
 
-		lengthSplit = [3, 3, 2, 2]
+		def check(m):
+			return m.author == ctx.author
 
-		InputRandomized = random.sample(groupArray, len(groupArray))
+		for i in range(1, 2):
+			await ctx.send(f"Please input team size!")
+			msg = await self.client.wait_for('message', check=check)
+			lengthSplit = msg.content.split(", ")
 
-		FinalizedRandom = [InputRandomized[x - y: x] for x, y in zip( 
-          accumulate(lengthSplit), lengthSplit)]
+			try:
+				# Turn lengthsplit array from str to int
+				for i in range(0, len(lengthSplit)): 
+					lengthSplit[i] = int(lengthSplit[i])
 
-		for x in range(0, 4):
-			teamArray = []
-			await ctx.send(f'Group {x+1}')
+				totalSize = sum(lengthSplit)
+				
+				# print(totalSize)
+				# print(len(groupArray))
 
-			if(lengthSplit[x] == 2):
-				for y in range(0, 2):
-					teamArray.append(FinalizedRandom[x][y])
-			else:
-				for y in range(0, 3):
-					teamArray.append(FinalizedRandom[x][y])
+				if(totalSize == len(groupArray)):
+					# Randomizing groupArray's array
+					inputRandomized = random.sample(groupArray, len(groupArray))
 
-			await ctx.send(f"```{', '.join(map(str, teamArray))}```")
+					# Splitting randomized groupArray's array based on lengthSplit
+					finalizedRandom = [inputRandomized[x - y: x] for x, y in zip(accumulate(lengthSplit), lengthSplit)]
+
+					# Print Team
+					for x in range(0, len(lengthSplit)):
+						teamArray = []
+						await ctx.send(f'Group {x+1}')
+
+						for y in range(0, lengthSplit[x]):
+							teamArray.append(finalizedRandom[x][y])
+
+						await ctx.send(f"```{', '.join(map(str, teamArray))}```")
+				elif(totalSize > len(groupArray)):
+					await ctx.send(f"Your group size is exceeding the number of available members! Please redo `.randomizeGroup` again.")
+				else:
+					await ctx.send(f"Your group size is not equal to the number of available members! Please redo `.randomizeGroup` again.")
+			except:
+				await ctx.send(f"Unknown input. Make sure your input is like `3, 3, 2, 2` (separated by `, `)")
 
 def setup(client):
 	client.add_cog(Misc(client))
